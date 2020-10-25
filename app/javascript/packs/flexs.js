f470d91a025f40924ae5f05d6c55f705727f4a0f
Serg <seregaproger@yandex.ru>
2020-10-25 22:46:54 +0300

    Cart, ref .js files, don't add item in Cart

--- app/javascript/packs/flexs.js
+++ app/javascript/packs/flexs.js
@@ -1,5 +1,5 @@
 // Can also be used with $(document).ready()
-$(window).load(function() {
+$(window).on('load', function () {
     $('.flexslider').flexslider({
         animation: "slide",
         controlNav: "thumbnails"
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -7,7 +7,8 @@ environment.plugins.prepend("Provide",
     new webpack.ProvidePlugin({
         $: 'jquery/src/jquery',
         jQuery: 'jquery/src/jquery'
+//        Bloodhound: 'typeahead.js/dist/bloodhound.js'
     })
 )
-
+//Bloodhound: 'typeahead.js/dist/bloodhound.js'
 module.exports = environment
